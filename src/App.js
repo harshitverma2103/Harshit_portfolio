5b13eb539597f2ce514effeb61979670bd005e83
harshitverma2103 <harshitverma2103@gmail.com>
2024-07-10 13:45:26 +0530

    introduce router , created navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Skills from "./pages/skills";
+import Contact from "./pages/contact";
+import Resume from "./pages/resume";
+import Navbar from "./components/navBar";
+import Portfolio from "./pages/portfolio";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
